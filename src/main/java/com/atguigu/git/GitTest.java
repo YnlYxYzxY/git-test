587a3714e170d66ff46c57123bf5218b803a0af2
zxy <1243454750@qq.com>
2022-06-28 17:03:48 +0800

    gitTest-push-github

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello,git4-hot-fix");
         System.out.println("hello,git4-master");
         System.out.println("hello, hot-fix");
+        System.out.println("hello, git-hub-push");
     }
 
 }
