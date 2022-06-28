6419f3f70455148672a30e6aa286840dfbd74853
张晓宇 <66678442+YnlYxYzxY@users.noreply.github.com>
2022-06-28 17:14:09 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hello,git4-master");
         System.out.println("hello, hot-fix");
         System.out.println("hello, git-hub-push");
+        System.out.println("hello, git-hub-push远程更改pulltest");
     }
 
 }
